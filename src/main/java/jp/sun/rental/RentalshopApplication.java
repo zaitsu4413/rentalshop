15e8996693ad48ad51f68f41e79cc630b4aacf86
zaitu <granschool4413@gmail.com>
2026-01-28 10:09:46 +0900

    プロジェクトファイルの作成

---
+++ src/main/java/jp/sun/rental/RentalshopApplication.java
@@ -0,0 +1,13 @@
+package jp.sun.rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/sun/rental/RentalshopApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.sun.rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
